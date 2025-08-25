3ac477cbf31026acb22ed456626bd0c76faf6d59
Adam Gray <adam.gray@sanity.io>
2025-08-25 15:29:26 -0700

    pull news from sanity

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,62 +1,23 @@
+import { sanityFetch } from "@/sanity/client";
+import { urlFor } from "@/sanity/sanityImageUrl";
+import { defineQuery } from "next-sanity";
 import Image from "next/image";
 import Link from "next/link";
-const posts = [
-  {
-    id: "one",
-    image: "https://picsum.photos/600/400",
-    title: "Mastering AI-Powered Mixing: A Beginner's Tutorial for 2025 Tools",
-    category: "tutorial",
-  },
-  {
-    id: "two",
-    image: "https://picsum.photos/600/400?random=1",
-    title: "Top 5 Spatial Audio Plugins Reviewed: Immersive Sound on a Budget",
-    category: "review",
-  },
-  {
-    id: "three",
-    image: "https://picsum.photos/600/400?random=2",
-    title: "Major DAW Updates Announced at NAMM 2025",
-    category: "news",
-  },
-  {
-    id: "four",
-    image: "https://picsum.photos/600/400?random=3",
-    title: "Step-by-Step Guide to Cloud Collaboration in Music Production",
-    category: "tutorial",
-  },
-  {
-    id: "five",
-    image: "https://picsum.photos/600/400?random=4",
-    title: "Sustainable Audio Gear - Eco-Friendly Microphones Tested",
-    category: "review",
-  },
-  {
-    id: "six",
-    image: "https://picsum.photos/600/400?random=5",
-    title: "Creating 'Dirty' Aesthetics with Vintage Effects Plugins",
-    category: "tutorial",
-  },
-  {
-    id: "seven",
-    image: "https://picsum.photos/600/400?random=6",
-    title: "Step-by-Step Guide to Cloud Collaboration in Music Production",
-    category: "tutorial",
-  },
-  {
-    id: "eight",
-    image: "https://picsum.photos/600/400?random=7",
-    title: "Sustainable Audio Gear - Eco-Friendly Microphones Tested",
-    category: "review",
-  },
-  {
-    id: "nine",
-    image: "https://picsum.photos/600/400?random=8",
-    title: "Creating 'Dirty' Aesthetics with Vintage Effects Plugins",
-    category: "tutorial",
-  },
-];
-export default function Home() {
+
+export default async function Home() {
+  const news = await sanityFetch({
+    query: defineQuery(
+      `
+      *[_type == "news"] {
+        _id,
+        title,
+        "image": mainImage,
+        "imageUrl": mainImage.asset->url
+      }
+      `,
+    ),
+  });
+
   return (
     <div>
       <nav className="container mx-auto p-8 flex justify-between items-center">
@@ -83,31 +44,35 @@ export default function Home() {
           <p>audio production tips, techniques, reviews and news</p>
         </div>
         <div className="grid grid-cols-3 gap-4 mb-20">
-          {posts.map((post) => (
+          {news.map((post) => (
             <Link
-              href={`/posts/${post.id}`}
-              key={post.id}
+              key={post._id}
+              href={`/posts/${post._id}`}
               className="relative group rounded"
             >
               <div className="relative">
                 <Image
-                  width={800}
+                  width={400}
                   height={400}
-                  src={post.image}
-                  alt={post.title}
-                  className="w-full rounded"
+                  src={urlFor(post.image)
+                    .width(400)
+                    .height(400)
+                    .quality(20)
+                    .url()}
+                  alt={post.image.alt}
+                  className="w-full rounded-lg"
                 />
                 <Image
-                  width={800}
+                  width={400}
                   height={400}
-                  src={post.image}
-                  alt={post.title}
+                  src={urlFor(post.image).width(10).height(10).quality(1).url()}
+                  alt={post.image.alt}
                   className="w-full blur-3xl absolute top-0 left-0 -z-10 group-hover:blur-2xl transition-all"
                 />
               </div>
-              <div className="p-4 rounded absolute bottom-0 h-full w-full left-0 flex flex-col justify-end group-hover:backdrop-blur backdrop-brightness-50 group-hover:backdrop-brightness-40 transition-all text-white">
+              <div className="p-4 rounded-lg absolute bottom-0 h-full w-full left-0 flex flex-col justify-end group-hover:backdrop-blur backdrop-brightness-50 group-hover:backdrop-brightness-40 transition-all text-white">
                 <div>
-                  <h6 className="uppercase text-xs">{post.category}</h6>
+                  <h6 className="uppercase text-xs">news</h6>
                   <h3 className="font-bold text-lg">{post.title}</h3>
                 </div>
               </div>
