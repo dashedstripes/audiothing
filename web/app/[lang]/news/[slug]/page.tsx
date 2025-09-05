377567ced36912d622997024d5afc433cad98e0d
Adam Gray <adam.gray@sanity.io>
2025-09-05 11:26:19 -0700

    translate on frontend

--- web/app/[lang]/news/[slug]/page.tsx
+++ web/app/[lang]/news/[slug]/page.tsx
@@ -4,33 +4,40 @@ import { defineQuery } from "next-sanity";
 import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import Header from "@/components/header";
+import { i18n } from "@/i18n-config";
 
 export async function generateStaticParams() {
   const news = await sanityFetch({
     query: defineQuery(
       `
-      *[_type == "news"] | order(_createdAt desc)[0..11] {
+      *[_type == "news" && (language in $locales)] | order(_createdAt desc)[0..11] {
         slug,
+        language,
       }
       `,
     ),
+    params: {
+      locales: i18n.locales,
+    },
   });
 
-  return news.map((item: { slug: { current: string } }) => ({
+  return news.map((item: { slug: { current: string }; language: string }) => ({
     slug: item.slug.current,
+    lang: item.language,
   }));
 }
 
 export default async function NewsPage({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string; lang: string }>;
 }) {
-  const { slug } = await params;
+  const { slug, lang } = await params;
+
   const news = await sanityFetch({
     query: defineQuery(
       `
-      *[_type == "news" && slug.current == $slug][0] {
+      *[_type == "news" && slug.current == $slug && language == $language][0] {
         _id,
         title,
         slug,
@@ -42,6 +49,7 @@ export default async function NewsPage({
     ),
     params: {
       slug,
+      language: lang,
     },
   });
 
--- web/app/[lang]/page.tsx
+++ web/app/[lang]/page.tsx
@@ -1,15 +1,21 @@
 import Header from "@/components/header";
 import PostsChunk from "@/components/posts-chunk";
+import { Locale } from "@/i18n-config";
 import { HOME_POST_CHUNKResult } from "@/sanity.types";
 import { sanityFetch } from "@/sanity/client";
 import { HOME_POST_CHUNK } from "@/sanity/queries";
 import { defineQuery } from "next-sanity";
 
-export default async function Home() {
+export default async function Home(props: {
+  params: Promise<{ lang: Locale }>;
+}) {
+  const { lang } = await props.params;
+
   const posts: HOME_POST_CHUNKResult = await sanityFetch({
     query: defineQuery(HOME_POST_CHUNK),
     params: {
       lastCreatedAt: null,
+      language: lang,
     },
   });
 
