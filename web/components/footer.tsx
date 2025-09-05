5a89d91b96cebc5401d619d69728a72f2509922e
Adam Gray <adam.gray@sanity.io>
2025-09-05 11:32:06 -0700

    quit complaining vercel

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
@@ -5,17 +6,17 @@ export default function Footer() {
   return (
     <footer className="py-20 text-center text-sm text-gray-800 mt-auto">
       <div className="mb-2.5">
-        <a href="/about" className="text-gray-800 no-underline mx-2.5">
+        <Link href="/about" className="text-gray-800 no-underline mx-2.5">
           About Us
-        </a>{" "}
+        </Link>{" "}
         |
-        <a href="/privacy" className="text-gray-800 no-underline mx-2.5">
+        <Link href="/privacy" className="text-gray-800 no-underline mx-2.5">
           Privacy
-        </a>{" "}
+        </Link>{" "}
         |
-        <a href="/advertise" className="text-gray-800 no-underline mx-2.5">
+        <Link href="/advertise" className="text-gray-800 no-underline mx-2.5">
           Advertise
-        </a>
+        </Link>
       </div>
       <div>
         © {year} <span className="font-bold">audio</span>thing
